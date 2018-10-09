db1f300d285e294342936046ab60b124d46d35fc
samesh3mikha <samesh3mikha@gmail.com>
2018-10-09 14:43:36 +1000

    Create empty wireframe for event list scene

--- Bridj/AppDelegate.swift
+++ Bridj/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setupRootViewController()
         return true
     }
 
@@ -41,6 +41,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    // MARK: - Private Helper
+    
+    private func setupRootViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = EvenListViewController(nibName: nil, bundle: Bundle(for: EvenListViewController.self))
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
 }
 
---
+++ Bridj/Presentation/Scenes/EventList/EvenListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EvenListViewController.swift
+//  Bridj
+//
+//  Created by samesh swongamikha on 10/9/18.
+//  Copyright © 2018 samesh swongamikha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class EvenListViewController: UIViewController {
+    
+}
