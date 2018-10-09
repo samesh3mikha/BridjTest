b93671dd151b034bfa3b3d00e65d6fc5442f48e0
samesh3mikha <samesh3mikha@gmail.com>
2018-10-09 14:04:12 +1000

    Initial Commit

---
+++ Bridj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bridj
+//
+//  Created by samesh swongamikha on 10/9/18.
+//  Copyright © 2018 samesh swongamikha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
